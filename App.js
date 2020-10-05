25eae40b9312273d4f5fbb62a8b3c11f8f111f27
faizanshar <muhammadfaizanshar@gmai.com>
2020-10-05 21:13:28 +0700

    Tugas(loter)

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Login from  './src/components/Login'
+import Register from './src/components/Register'
+const App = () => {
+  return (
+    <>
+      <Login/>
+      {/* <Register/> */}
+    </>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
